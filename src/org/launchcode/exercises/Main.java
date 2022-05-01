bad1ea3c9f63e7e7f3e801c4fa774f80955aad65
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-04-30 20:36:59 -0500

    working on exercises but CheckFileExtention is still broken

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        Divide(20, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +17,43 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> student: studentFiles.entrySet()) {
+            CheckFileExtention(student.getValue());
+        }
+
     }
 
-//    public static void Divide(int x, int y)
-//    {
-//        // Write code here!
-//    }
-//
-//    public static int CheckFileExtension(String fileName)
-//    {
-//        // Write code here!
-//    }
+    public static void Divide(int x, int y) {
+        // Write code here!
+        if (y <= 0) {
+            try {
+                throw new ArithmeticException("Enter a positive integer for total points possible. Zero cannot be in the denominator");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public static int CheckFileExtension(String fileName) {
+        // Write code here!
+        int score = 0;
+
+        if (fileName.contains(".java")) {
+            score = 1;
+        }
+
+        if (!fileName.contains(".java")) {
+            score = 0;
+        }
+
+        if (fileName.isBlank()) {
+            try {
+                throw new RuntimeException("fileName is either an empty string or null");
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
+            score = -1;
+        }
+        return score;
+    }
 }
---
+++ src/org/launchcode/exercises/zeroDenominatorException.java
@@ -0,0 +1,8 @@
+package org.launchcode.exercises;
+
+public class zeroDenominatorException extends Exception {
+
+    public zeroDenominatorException(String message) {
+        super(message);
+    }
+}
